bae388232b8b06490821df2531b5f2e612ef5769
uniquestepoutfits <uniquestepoutfits@gmail.com>
2026-07-15 02:38:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+const wheel = document.getElementById("wheel");
+
+const prizes = [
+    "💸 ৳20 Cashback",
+    "💸 ৳30 Cashback",
+    "💸 ৳40 Cashback",
+    "💸 ৳50 Cashback",
+    "💸 ৳100 Cashback",
+    "🎟️ ৳150 Voucher",
+    "🚚 Free Delivery",
+    "👕 Free Premium Shirt"
+];
+
+let spinning = false;
+
+function spinWheel(){
+
+    if(spinning) return;
+
+    spinning = true;
+
+    const randomIndex = Math.floor(Math.random()*prizes.length);
+
+    const degree = 3600 + (randomIndex * 45);
+
+    wheel.style.transform = `rotate(-${degree}deg)`;
+
+    setTimeout(()=>{
+
+        alert("🎉 Congratulations!\n\nYou won:\n\n"+prizes[randomIndex]);
+
+        spinning = false;
+
+    },5000);
+
+}
